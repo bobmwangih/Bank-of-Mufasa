f98e713b29ade770e9b80c6e69bece68c2bba401
bobmwangih <bobbymwangih@gmail.com>
2022-09-30 12:03:30 +0300

    spring security impl -Client credentials grant flow

---
+++ GatewayServer/src/main/java/com/mufasa/gatewayServer/GatewayServerApplication.java
@@ -0,0 +1,38 @@
+package com.mufasa.gatewayServer;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/mufasa/accounts/**")
+						.filters(f -> f.rewritePath("/mufasa/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p.path("/mufasa/loans/**")
+						.filters(f -> f.rewritePath("/mufasa/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p.path("/mufasa/cards/**")
+						.filters(f -> f.rewritePath("/mufasa/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://CARDS"))
+				.build();
+	}
+
+}
