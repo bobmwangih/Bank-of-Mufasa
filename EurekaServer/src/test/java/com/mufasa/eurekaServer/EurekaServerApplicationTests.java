58a29783af7c45249ccfa8dcddc37dadfc6442fb
bobmwangih <bobbymwangih@gmail.com>
2022-10-03 10:08:18 +0300

    EurekaServer

---
+++ EurekaServer/src/test/java/com/mufasa/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mufasa.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
