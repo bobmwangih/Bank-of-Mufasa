878ca176f509c1420952f420f099721f5ec70ab9
bobmwangih <bobbymwangih@gmail.com>
2022-10-03 10:08:58 +0300

    gatewayserver

--- GatewayServer/src/main/java/com/mufasa/gatewayServer/configs/SecurityConfig.java
+++ GatewayServer/src/main/java/com/mufasa/gatewayServer/configs/SecurityConfig.java
@@ -2,16 +2,10 @@ package com.mufasa.gatewayServer.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,16 +13,18 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http) {
-		http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").hasRole("ACCOUNTS")
+		//http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").hasRole("ACCOUNTS")
+		http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").authenticated()
 				.pathMatchers("/mufasa/cards/**").authenticated().pathMatchers("/mufasa/loans/**").permitAll())
-				.oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+				//.oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+		.oauth2Login(Customizer.withDefaults());
 		http.csrf().disable();
 		return http.build();
 	}
 
-	Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-		return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
-	}
+//	Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+//		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+//		return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+//	}
 }
