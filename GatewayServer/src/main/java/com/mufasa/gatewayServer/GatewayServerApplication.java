a81fc3742cf44fa1c4939d14622d091f030c655f
bobmwangih <bobbymwangih@gmail.com>
2022-10-11 14:51:24 +0300

    oauth2 client grant flow

--- GatewayServer/src/main/java/com/mufasa/gatewayServer/GatewayServerApplication.java
+++ GatewayServer/src/main/java/com/mufasa/gatewayServer/GatewayServerApplication.java
@@ -1,9 +1,9 @@
 package com.mufasa.gatewayServer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -17,32 +17,24 @@ public class GatewayServerApplication {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
-	@Autowired
-	private TokenRelayGatewayFilterFactory filterFactory;
-
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p.path("/mufasa/accounts/**")
-						.filters(f -> f.filters(filterFactory.apply())
-								.rewritePath("/mufasa/accounts/(?<segment>.*)", "/${segment}")
-								// .filters(f -> f.rewritePath("/mufasa/accounts/(?<segment>.*)", "/${segment}")
-								// .addResponseHeader("X-Response-Time", new Date().toString()))
-								.removeRequestHeader("Cookie"))
-						.uri("lb://ACCOUNTS"))
-				.route(p -> p.path("/mufasa/loans/**").filters(
-						f -> f.filters(filterFactory.apply()).rewritePath("/mufasa/loans/(?<segment>.*)", "/${segment}")
-						// .filters(f -> f.rewritePath("/mufasa/loans/(?<segment>.*)", "/${segment}")
-//								.addResponseHeader("X-Response-Time", new Date().toString()))
-								.removeRequestHeader("Cookie"))
-						.uri("lb://LOANS"))
-				.route(p -> p.path("/mufasa/cards/**").filters(
-						f -> f.filters(filterFactory.apply()).rewritePath("/mufasa/cards/(?<segment>.*)", "/${segment}")
-						// .filters(f -> f.rewritePath("/mufasa/cards/(?<segment>.*)", "/${segment}")
-//								.addResponseHeader("X-Response-Time", new Date().toString()))
-								.removeRequestHeader("Cookie"))
-						.uri("lb://CARDS"))
-				.build();
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/mufasa/accounts/**")
+	            .filters(f -> f.rewritePath("/mufasa/accounts/(?<segment>.*)","/${segment}")
+	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .uri("lb://ACCOUNTS")).
+	        route(p -> p
+		            .path("/mufasa/loans/**")
+		            .filters(f -> f.rewritePath("/mufasa/loans/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://LOANS")).
+	        route(p -> p
+		            .path("/mufasa/cards/**")
+		            .filters(f -> f.rewritePath("/mufasa/cards/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://CARDS")).build();
 	}
 
 }
--- GatewayServer/src/main/java/com/mufasa/gatewayServer/configs/SecurityConfig.java
+++ GatewayServer/src/main/java/com/mufasa/gatewayServer/configs/SecurityConfig.java
@@ -2,29 +2,37 @@ package com.mufasa.gatewayServer.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-	@Bean
-	public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http) {
-		//http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").hasRole("ACCOUNTS")
-		http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").authenticated()
-				.pathMatchers("/mufasa/cards/**").authenticated().pathMatchers("/mufasa/loans/**").permitAll())
-				//.oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
-		.oauth2Login(Customizer.withDefaults());
-		http.csrf().disable();
-		return http.build();
-	}
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.pathMatchers("/mufasa/accounts/**").authenticated()
+        		//.hasRole("ACCOUNTS")
+                        .pathMatchers("/mufasa/cards/**").authenticated()
+                        .pathMatchers("/mufasa/loans/**").permitAll())
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+        http.csrf().disable();
+        return http.build();
+    }
 
-//	Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-//		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-//		return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
-//	}
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
